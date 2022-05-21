62fefb2dab86f8d9de280e69e0e8f695ea665044
Adrix-fr <105988790+Adrix-fr@users.noreply.github.com>
2022-05-21 20:25:48 +0200

    Update interactionCreate.js

--- Events/interaction/interactionCreate.js
+++ Events/interaction/interactionCreate.js
@@ -1,4 +1,8 @@
-const { Client, CommandInteraction, MessageEmbed } = require('discord.js');
+const {
+    Client,
+    CommandInteraction,
+    MessageEmbed
+} = require('discord.js');
 
 module.exports = {
     name: "interactionCreate",
@@ -8,14 +12,16 @@ module.exports = {
      * @param {CommandInteraction} interaction 
      * @param {Client} client 
      */
-    async execute(interaction, client){
-        if(interaction.isCommand()){
+    async execute(interaction, client) {
+        if (interaction.isCommand()) {
             const command = client.commands.get(interaction.commandName);
-            if(!command) return interaction.reply({embeds: [
-                new MessageEmbed()
-                .setColor("RED")
-                .setDescription("⛔ Une erreur s'est produite lors de l'exécution de cette commande.")
-            ]}) && client.commands.delete(interaction.commandName)
+            if (!command) return interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setColor("RED")
+                    .setDescription("⛔ Une erreur s'est produite lors de l'exécution de cette commande.")
+                ]
+            }) && client.commands.delete(interaction.commandName)
             command.execute(interaction, client)
         }
     }
