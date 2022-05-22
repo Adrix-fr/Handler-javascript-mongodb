c0efb02abceb74579023d5336e24a85bc0f07472
Adrix-fr <105988790+Adrix-fr@users.noreply.github.com>
2022-05-22 04:00:57 +0200

    Add files via upload

---
+++ Commands/util/help.js
@@ -0,0 +1,107 @@
+const {
+    CommandInteraction,
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton
+} = require('discord.js');
+const {
+    Client
+} = require('discord.js')
+module.exports = {
+        name: "help",
+        description: "Permet de donner les commandes du bot",
+        directory: "util",
+
+        /**
+         * @param {CommandInteraction} interaction
+         * @param {Client} client
+         */
+        async execute(interaction, client) {
+
+            try {
+                if (!interaction.isCommand()) return;
+
+                await interaction.deferReply().catch((_) => {})
+
+                const dirs = [...new Set(client.commands.map((c) => c.directory))];
+
+                const helpArray = dirs.map((d) => {
+                    const getCmd = client.commands.filter((c) => c.directory === d).map((c) => {
+                        return {
+                            name: c.name || "Aucun nom",
+                            description: c.description || "Aucune description"
+                        }
+                    })
+                    return {
+                        name: d,
+                        commands: getCmd,
+                    }
+                })
+                let pageNo = 1;
+
+
+                const embed = new MessageEmbed()
+                    .setColor('BLUE')
+                    .setThumbnail(client.user.displayAvatarURL({
+                        size: 4096
+                    }))
+                    .setTimestamp()
+                    .setFooter({
+                        text: `Page ${pageNo}/${helpArray.length}`
+                    })
+
+                const getButtons = (pageNo) => {
+                    return new MessageActionRow().addComponents(
+                        new MessageButton().setLabel('précédent').setCustomId('prev').setStyle('DANGER').setDisabled(pageNo < 1),
+                        new MessageButton().setLabel('Suivant').setCustomId('next').setStyle('DANGER').setDisabled(!(pageNo < helpArray.length)),
+                    )
+                };
+
+                embed.setTitle(`**__Categorie :__** \`${helpArray[pageNo-1].name}\``).addFields(
+                    helpArray[0].commands.map(({ name, description }) => {
+                        console.log(name, description)
+                        return {
+                            name: `\`${name}\``,
+                            value: `${description}`,
+                            inline: true
+                        };
+                    })
+                    );
+
+                    const intrMsg = await interaction.editReply({
+                        embeds: [embed],
+                        components: [getButtons(pageNo)],
+                        fetchReply: true
+                    })
+
+                    const collector = intrMsg.createMessageComponentCollector({time: 600000, componentType: "BUTTON"});
+
+                    collector.on("collect", async i => {
+                        if(i.customId === "next"){
+                            pageNo++;
+                        } else if (i.customId === "prev"){
+                            pageNo--;
+                        }
+
+                        const categ = helpArray[pageNo-1];
+
+                        embed.fields = [];
+                        embed.setTitle(`**__Categorie :__** \`${categ.name}\``).addFields(
+                            categ.commands.map(({ name, description }) => {
+                                console.log(name, description)
+                                return {
+                                    name: `\`${name}\``,
+                                    value: `${description}`,
+                                    inline: true
+                                };
+                            })
+                            ).setFooter({text: `Page ${pageNo}/${helpArray.length}`});
+
+                            await i.update({embeds: [embed], components: [getButtons(pageNo)], fetchReply: true})
+                    })
+                }
+                catch (err) {
+                    console.log("Quelque chose a mal tourné", err)
+                }
+            }
+        }
---
+++ Commands/util/status.js
@@ -0,0 +1,49 @@
+const {
+    Client,
+    MessageEmbed,
+    CommandInteraction
+} = require('discord.js');
+
+const {
+    connection
+} = require('mongoose');
+
+require('../../Events/client/Ready');
+
+module.exports = {
+    name: "status",
+    description: "Montre le statut du client via la database",
+    directory: "util",
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        const Response = new MessageEmbed()
+            .setColor("AQUA")
+            .setDescription(`> **Client**: \`🟢 ONLINE\` - \`${client.ws.ping}ms\`\n> **Uptime**: <t:${parseInt(client.readyTimestamp / 1000)}:R>\n> **DataBase**: \`${switchTo(connection.readyState)}\``)
+        interaction.reply({
+            embeds: [Response]
+        })
+    }
+}
+
+function switchTo(val) {
+    var status = " ";
+    switch (val) {
+        case 0:
+            status = `🔴 DISCONNECTED`
+            break;
+        case 1:
+            status = `🟢 CONNECTED`
+            break;
+        case 2:
+            status = `🟠 CONNECTING`
+            break;
+        case 3:
+            status = `🟣 DISCONNECTING`
+            break;
+    }
+    return status
+}
